d48ce293282fb606fc30d0f4950f765c9139b270
frsearto <frsearto@gmail.com>
2026-05-10 16:19:22 -0400

    commit

--- src/main/java/com/duoc/backend/Patient/PatientController.java
+++ src/main/java/com/duoc/backend/Patient/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -22,7 +23,7 @@ public class PatientController {
 
     @GetMapping("/register")
     public String greetings(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Hello {" + name + "}";
+        return "Hello {" + HtmlUtils.htmlEscape(name) + "}";
     }
 
     @GetMapping
--- src/main/java/com/duoc/backend/SecuredController.java
+++ src/main/java/com/duoc/backend/SecuredController.java
@@ -3,12 +3,13 @@ package com.duoc.backend;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 @RestController
 public class SecuredController {
 
     @RequestMapping("greetings")
     public String greetings(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Hello {" + name + "}";
+        return "Hello {" + HtmlUtils.htmlEscape(name) + "}";
     }
 }
